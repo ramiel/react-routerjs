9dff8486d52c56a2559a308eb75f6cab5c938366
Fabrizio Ruggeri <fabrizio.ruggeri@gmail.com>
2019-08-14 11:36:50 +0200

    Remove dist folder

--- dist/Link.d.ts
+++
@@ -1,3 +0,0 @@
-import React, { AnchorHTMLAttributes } from 'react';
-declare const Link: React.SFC<AnchorHTMLAttributes<HTMLAnchorElement>>;
-export default Link;
--- dist/index.d.ts
+++
@@ -1,3 +0,0 @@
-import RouterProvider from './RouterProvider';
-import Link from './Link';
-export { Link, RouterProvider };
