6e78988f8c5890acaaff77bdd542ce3b0fd7396b
Fabrizio Ruggeri <fabrizio.ruggeri@gmail.com>
2019-10-16 15:03:12 +0200

    Hook always return the router context or an error

--- src/useRouter.ts
+++ src/useRouter.ts
@@ -3,6 +3,11 @@ import { RouterContext } from './RouterProvider';
 
 const useRouter = () => {
   const routerContext = useContext(RouterContext);
+  if (!routerContext) {
+    throw new Error(
+      '"useRouter" hook can be used only in components descendants of <RouterProvider>',
+    );
+  }
   return routerContext;
 };
 
